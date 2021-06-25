da54625af769352128c4834ef7bffe5531d1400c
Lucas Nascimento <lnascimento123489@gmail.com>
2021-06-24 23:13:12 -0300

    Autenticação do usuario | aula 2 completa

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import { BrowserRouter ,Route } from 'react-router-dom';
+
+import { AuthContextProvider } from './contexts/AuthContext';
+
+import { Home } from './pages/Home';
+import { NewRoom } from './pages/NewRoom';
+
+import './styles/global.scss';
 
 function App() {
   return (
-    <h1>Simbora!!!</h1>
+    <BrowserRouter>
+     <AuthContextProvider>
+        <Route path='/' exact component={Home} />
+        <Route path='/rooms/new' component={NewRoom} />
+      </AuthContextProvider>
+    </BrowserRouter>    
   );
 }
 
